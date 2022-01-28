328df453a98d60e662d820eef688e7b4b22c2ae1
DZuko <dcouser.dev@gmail.com>
2022-01-28 11:53:11 -0500

    added natspec to swapAndTransferUnwrappedNatoSupportingFeeOnTransferTokensWithV2, ExactInputSingleParams and swapAndTransferUnwrappedNatoWithV3

--- src/AbacusV0.sol
+++ src/AbacusV0.sol
@@ -30,7 +30,7 @@ contract AbacusV0 {
     ISwapRouter UniV3Router;
 
     /// @notice Wrapped native token (WNATO) address for the chain. This address will change depending on the chain the contract is deployed to (Ex. WETH for Ethereum L1, WMATIC for Polygon, WBNB for BSC).
-    address public constant WNATO_ADDRESS;
+    address public wnatoAddress;
 
     /// @notice Wrapped ETH contract instance to unwrap WETH to ETH. While this variable is named WETH, it could be any native token depending on the chain the contract is deployed to (Ex. WETH for Ethereum L1, WMATIC for Polygon, WBNB for BSC).
     WETH private _wnato;
@@ -64,10 +64,10 @@ constructor(address _wnatoAddress, address _uniV2Router, address _uniV3Router){
     abacusFeeMul1000=25;
 
     /// @notice Initialize the wrapped native token address for the chain.
-    WNATO_ADDRESS=_wnatoAddress;
+    wnatoAddress=_wnatoAddress;
 
     /// @notice Initialize the wrapped ETH contract instance with the wrapped native token address for the chain.
-    _wnato=WETH(_wnatoAddress);
+    _wnato=WETH(payable(_wnatoAddress));
 
     /// @notice Initialize the UniV2Router address.
     UniV2RouterAddress=_uniV2Router;
@@ -76,43 +76,39 @@ constructor(address _wnatoAddress, address _uniV2Router, address _uniV3Router){
     UniV3RouterAddress=_uniV3Router;
 
     /// @notice Initialize the UniV2Router contract instance.
-    UniV2Router = IUniV2Router(_uniV2Router);
+    UniV2Router = IUniswapV2Router02(_uniV2Router);
 
     /// @notice Initialize the UniV3Router contract instance.
     UniV3Router = ISwapRouter(_uniV3Router);
-
-
-    
 }
 
-
-    struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 deadline;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-    }
-
-/// @notice swap and transfer to the chain's native token
+/// @notice Modifier that checks if the msg.sender is the owner of the contract. The only functions that are set as onlyOwner() are setAbacusFee, setAbacusWallet and transferOwnership.
 modifier onlyOwner() {
     require(msg.sender==_owner, "!owner");
     _;
 }
 
-/// @notice 
-
-/// @dev change this natspec description but mention approve swap router, not the contract to save gas from unnecessary transfers
+/// @notice This function uses a UniV2 compatible router to swap a token for the wrapped native token, unwraps the native token and sends the amountOut minus the abacus fee back to the msg.sender.
+/// @param _callData This param is abi encoded bytes containing the amountIn and the amountOutMin for the swap, the token to swap from and the transaction deadline for the swap.  
+/// @dev To create the abi encoded calldata, simply use abi.encode(_amountIn,_amountOutMin,_tokenToSwap,_deadline).
+/// @dev _amountIn is the exact amount of tokens you want to swap.
+/// @dev _amountOutMin is the minimum amount of tokens you want resulting from the swap.
+/// @dev _tokenToSwap is the address of the token that you want to swap from (Ex. When swapping from $LINK to $ETH, _tokenToSwap is the address for $LINK).
+/// @dev _deadline is the unix time after which a swap will fail, to protect against long-pending transactions and wild swings in prices.
+/// @dev The swap router must be approved for this to function to succeed. Since tokens are never sent to the Abacus contract before the swap, the Abacus does not need to be approved. 
+/// @dev This contract saves gas by only having to send the tokens to the router vs sending tokens to the contract, and then sending tokens to the router.
 function swapAndTransferUnwrappedNatoWithV2 (bytes calldata _callData) external {
 
-    /// @notice unpack the call data.
+    /// @notice Decode the call data.
     (uint _amountIn, uint _amountOutMin, address _tokenToSwap, uint _deadline) = abi.decode(_callData, (uint, uint, address, uint));
 
+    /// @notice Set the routing path for the swap to be _tokenToSwap to wnatoAddress
+    address[] memory path = new address[](2);
+    path[0]=_tokenToSwap;
+    path[1]=wnatoAddress;
+
     /// @notice Swap tokens for wrapped native tokens (nato).
-    uint amountRecieved = UniV2Router.swapExactTokensForTokens(_amountIn, _amountOutMin, [_tokenToSwap, WETH_ADDRESS], address(this), _deadline)[1];
+    uint amountRecieved = UniV2Router.swapExactTokensForTokens(_amountIn, _amountOutMin, path, address(this), _deadline)[1];
 
     /// @notice Unwrap wrapped nato to nato for the amount recieved from the swap.
     _wnato.withdraw(amountRecieved);
@@ -128,52 +124,89 @@ function swapAndTransferUnwrappedNatoWithV2 (bytes calldata _callData) external 
 }
 
 
+/// @notice This function uses a UniV2 compatible router to swap a token supporting fee on transfer tokens for the wrapped native token, unwraps the native token and sends the amountOut minus the abacus fee back to the msg.sender.
+/// @param _callData This param is abi encoded bytes containing the amountIn and the amountOutMin for the swap, the token to swap from and the transaction deadline for the swap.  
+/// @dev To create the abi encoded calldata, simply use abi.encode(_amountIn,_amountOutMin,_tokenToSwap,_deadline).
+/// @dev _amountIn is the exact amount of tokens you want to swap.
+/// @dev _amountOutMin is the minimum amount of tokens you want resulting from the swap.
+/// @dev _tokenToSwap is the address of the token that you want to swap from (Ex. When swapping from $LINK to $ETH, _tokenToSwap is the address for $LINK).
+/// @dev _deadline is the unix time after which a swap will fail, to protect against long-pending transactions and wild swings in prices.
+/// @dev The swap router must be approved for this to function to succeed. Since tokens are never sent to the Abacus contract before the swap, the Abacus does not need to be approved. 
+/// @dev This contract saves gas by only having to send the tokens to the router vs sending tokens to the contract, and then sending tokens to the router.
+function swapAndTransferUnwrappedNatoSupportingFeeOnTransferTokensWithV2 (bytes calldata _callData) external {
 
-/// @notice swap and transfer to the chain's native token
-function swapAndTransferUnwrappedNatoSupportingFeeOnTransferTokensWithV2 (address _lp) external {
-    //unpack the call data
+    /// @notice Decode the call data.
     (uint _amountIn, uint _amountOutMin, address _tokenToSwap, uint _deadline) = abi.decode(_callData, (uint, uint, address, uint));
 
-    //swap tokens for weth
-    uint amountRecieved = UniV3Router.swapExactTokensForTokensSupportingFeeOnTransferTokens(_amountIn, _amountOutMin, [_tokenToSwap, WETH_ADDRESS], address(this), _deadline)[1];
 
-    //unwrap weth
+    /// @notice Set the routing path for the swap to be _tokenToSwap to wnatoAddress
+    address[] memory path = new address[](2);
+    path[0]=_tokenToSwap;
+    path[1]=wnatoAddress;
+
+    /// @notice Swap tokens supporting fee on transfer tokens for wrapped native tokens (nato).
+    uint amountRecieved = UniV2Router.swapExactTokensForTokensSupportingFeeOnTransferTokens(_amountIn, _amountOutMin, path, address(this), _deadline)[1];
+
+    /// @notice Unwrap wrapped nato to nato for the amount recieved from the swap.
     _wnato.withdraw(amountRecieved);
 
-    //calculate the amount out less abacus fee
+    /// @notice Calculate the payout less abacus fee.
     (uint payout, uint abacusFee) = calculatePayoutLessAbacusFee(amountRecieved);
 
-    //send the amount out less abacus fee to the msg.sender
+    /// @notice Send the payout (amount out less abacus fee) to the msg.sender
     SafeTransferLib.safeTransferETH(msg.sender, payout);
 
-    //send the abacus fee to the _abacusWallet wallet
+    /// @notice Send the abacus fee to the abacus wallet.
     SafeTransferLib.safeTransferETH(_abacusWallet, abacusFee);
 }
 
 
-/// @notice swap and transfer to the chain's native token with V3 interface
+/// @notice Struct to send data to the Uniswap V3 Router exactInputSingle method.
+/// @dev _tokenIn is the contract address of the inbound token.
+/// @dev _tokenOut is the contract address of the outbound token.
+/// @dev _fee is the fee tier of the pool, used to determine the correct pool contract in which to execute the swap.
+/// @dev _recipient is the destination address of the outbound token.
+/// @dev _deadline is the unix time after which a swap will fail, to protect against long-pending transactions and wild swings in prices.
+/// @dev _amountIn is the exact amount of tokens you want to swap.
+/// @dev _amountOutMinimum is the minimum amount of tokens you want resulting from the swap.
+/// @dev _sqrtPriceLimitX96 can be used to set the limit for the price the swap will push the pool to, which can help protect against price impact or for setting up logic in a variety of price-relevant mechanisms
+struct ExactInputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint24 fee;
+    address recipient;
+    uint256 deadline;
+    uint256 amountIn;
+    uint256 amountOutMinimum;
+    uint160 sqrtPriceLimitX96;
+    }
+
+
+/// @notice This function uses the UniV3 router to swap a token for the wrapped native token, unwraps the native token and sends the amountOut minus the abacus fee back to the msg.sender.
+/// @notice This function utilizes the ISwapRouter.exactInputSingle function which swaps an exact amount of token A for the maximum amount of token B.
+/// @param _callData This param is abi encoded bytes containing the ExactInputSingleParams struct required to interact with the ISwapRouter exactInputSingle method.  
+/// @dev To create the abi encoded calldata, simply use abi.encode(_tokenIn,_tokenOut,_fee,_recipient,_deadline,_amountIn,_amountOutMinimum,_sqrtPriceLimitX96).
+/// @dev See ExactInputSingleParams struct for a definition of each argument that is encoded in the _callData.
+/// @dev The swap router must be approved for this to function to succeed. Since tokens are never sent to the Abacus contract before the swap, the Abacus does not need to be approved. 
+/// @dev This contract saves gas by only having to send the tokens to the router vs sending tokens to the contract, and then sending tokens to the router.
 function swapAndTransferUnwrappedNatoWithV3 (bytes calldata _callData) external {
-        (address tokenIn,
-        address tokenOut,
-        uint24 fee,
-        address recipient,
-        uint256 deadline,
-        uint256 amountIn,
-        uint256 amountOutMinimum,
-        uint160 sqrtPriceLimitX96) = abi.decode(_callData, (address,address,uint24,address,uint256,uint256,uint256,uint160));
 
-     uint amountRecieved = IUniV3Router.exactInputSingle(ExactInputSingleParams(tokenIn, WNATO_ADDRESS, fee, address(this), deadline, amountIn, amountOutMinimum, sqrtPriceLimitX96));
+    /// @notice 
+    (address _tokenIn,address _tokenOut,uint24 _fee,address _recipient,uint256 _deadline,uint256 _amountIn, uint256 _amountOutMinimum, uint160 _sqrtPriceLimitX96) = abi.decode(_callData, (address,address,uint24,address,uint256,uint256,uint256,uint160));
 
-    //unwrap weth
+    /// @notice
+    uint amountRecieved = ISwapRouter.exactInputSingle(ExactInputSingleParams(_tokenIn, wnatoAddress, _fee, address(this), _deadline, _amountIn, _amountOutMinimum, _sqrtPriceLimitX96));
+
+    /// @notice Unwrap wrapped nato to nato for the amount recieved from the swap.
     _wnato.withdraw(amountRecieved);
 
-    //calculate the amount out less abacus fee
+    /// @notice Calculate the payout less abacus fee.
     (uint payout, uint abacusFee) = calculatePayoutLessAbacusFee(amountRecieved);
 
-    //send the amount out less abacus fee to the msg.sender
+    /// @notice Send the payout (amount out less abacus fee) to the msg.sender
     SafeTransferLib.safeTransferETH(msg.sender, payout);
 
-    //send the abacus fee to the _abacusWallet wallet
+    /// @notice Send the abacus fee to the abacus wallet.
     SafeTransferLib.safeTransferETH(_abacusWallet, abacusFee);
 }
 
@@ -182,18 +215,19 @@ function calculatePayoutLessAbacusFee(uint amountOut) private view returns (uint
     return ((amountOut-abacusFee), abacusFee);
 }
 
-function approveAllSwapRouters(address _tokenAddress){
+function approveAllSwapRouters(address _tokenAddress) external {
     approveUniV2Router(_tokenAddress);
     approveUniV3Router(_tokenAddress);
 }
 
 
-function approveUniV2Router(address _tokenAddress){
-    ERC20(_tokenAddress).approve(UniV2RouterAddress, amount);
+//This may be convienent but its more gas efficient to create a contract instance of the token off chain and then approve the univ2 router
+function approveUniV2Router(address _tokenAddress, uint _amount) public {
+    ERC20(_tokenAddress).approve(UniV2RouterAddress, _amount);
 }
-
-function approveUniV3Router(address _tokenAddress){
-    ERC20(_tokenAddress).approve(UniV3RouterAddress, amount);
+//This may be convienent but its more gas efficient to create a contract instance of the token off chain and then approve the univ3 router
+function approveUniV3Router(address _tokenAddress, uint _amount) public {
+    ERC20(_tokenAddress).approve(UniV3RouterAddress, _amount);
 }
 
 
