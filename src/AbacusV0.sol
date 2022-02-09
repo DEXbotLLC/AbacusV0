5de8d21b52ec6cf7b6e12b79b72804812169f0f1
DZuko <dcouser.dev@gmail.com>
2022-02-09 15:27:56 -0500

    updated natspec, removed unused code, implemented efficiencies, calculated call data cost.

--- src/AbacusV0.sol
+++ src/AbacusV0.sol
@@ -38,7 +38,6 @@ contract AbacusV0 {
 
 /// @notice Constructor to initialize the contract on deployment.
 /// @param _wnatoAddress The wrapped native token address (Ex. WETH for Ethereum L1, WMATIC for Polygon, WBNB for BSC).
-
 constructor(address _wnatoAddress){
    
     /// @notice Set the owner of the contract to the msg.sender that deployed the contract.
@@ -68,16 +67,12 @@ fallback() external payable {
 
 
 /// @notice This function uses a UniV2 compatible router to swap a token for the wrapped native token, unwraps the native token and sends the amountOut minus the abacus fee back to the msg.sender.
-/// @dev To create the abi encoded calldata, simply use abi.encode(_amountIn,_amountOutMin,_tokenToSwap,_deadline).
 /// @param _lp is the v2 liquidity pool address for the _tokenIn and the wrapped native token for the chain.
 /// @param _amountIn is the exact amount of tokens you want to swap.
 /// @param _amountOutMin is the minimum amount of tokens you want resulting from the swap.
 /// @param _tokenIn is the address of the token that you want to swap from (Ex. When swapping from $LINK to $ETH, _tokenToSwap is the address for $LINK).
-/// @param _customAbacusFee boolean that tells the abacus to check for a custom fee. If false, the abacus will use the global abacus fee.
-/// @dev The swap router must be approved for this to function to succeed. Since tokens are never sent to the Abacus contract before the swap, the Abacus does not need to be approved. 
-/// @dev This contract saves gas by only having to send the tokens to the router vs sending tokens to the contract, and then sending tokens to the router.
+/// @param _customAbacusFee boolean that tells the abacus to check for a custom fee. If false, the abacus will use the global abacus fee set during the deployment of the contract.
 
-/// @dev The uniswapV2 Library usually calculates get amounts out, however this is calculated off chain to save gas
 function swapAndTransferUnwrappedNatoWithV2 (address _lp, uint _amountIn, uint _amountOutMin, address _tokenIn, bool _customAbacusFee) external {
 
     /// transfer the tokens to the lp
@@ -119,13 +114,11 @@ function swapAndTransferUnwrappedNatoWithV2 (address _lp, uint _amountIn, uint _
 }
 
 /// @notice This function uses a UniV2 compatible router to swap a token supporting fee on transfer tokens for the wrapped native token, unwraps the native token and sends the amountOut minus the abacus fee back to the msg.sender.
-/// @dev To create the abi encoded calldata, simply use abi.encode(_amountIn,_amountOutMin,_tokenToSwap,_deadline).
-/// @dev _amountIn is the exact amount of tokens you want to swap.
-/// @dev _amountOutMin is the minimum amount of tokens you want resulting from the swap.
-/// @dev _tokenIn is the address of the token that you want to swap from (Ex. When swapping from $LINK to $ETH, _tokenToSwap is the address for $LINK).
-/// @dev _deadline is the unix time after which a swap will fail, to protect against long-pending transactions and wild swings in prices.
-/// @dev The swap router must be approved for this to function to succeed. Since tokens are never sent to the Abacus contract before the swap, the Abacus does not need to be approved. 
-/// @dev This contract saves gas by only having to send the tokens to the router vs sending tokens to the contract, and then sending tokens to the router.
+/// @param _lp is the v2 liquidity pool address for the _tokenIn and the wrapped native token for the chain.
+/// @param _amountIn is the exact amount of tokens you want to swap.
+/// @param _amountOutMin is the minimum amount of tokens you want resulting from the swap.
+/// @param _tokenIn is the address of the token that you want to swap from (Ex. When swapping from $LINK to $ETH, _tokenToSwap is the address for $LINK).
+/// @param _customAbacusFee boolean that tells the abacus to check for a custom fee. If false, the abacus will use the global abacus fee set during the deployment of the contract.
 function swapAndTransferUnwrappedNatoSupportingFeeOnTransferTokensWithV2 (address _lp, uint _amountIn, uint _amountOutMin, address _tokenIn, bool _customAbacusFee) external {
 
     // transfer the tokens to the lp
@@ -196,14 +189,6 @@ function calculatePayoutLessAbacusFee(uint _amountOut, address _token, bool _cus
 }
 
 
-// /// @notice Function to update abacus fee. The fee can be set to any value between 0% and 3%. This contract is hard coded to never be able to set the fee to be greater than 3%
-// /// @dev The abacus fee is divided by 1000 when calculating the fee amount to effectively use float point calculations.
-// function setAbacusFee(uint _abacusFeeMul1000) external onlyOwner() {
-//     require(_abacusFeeMul1000>=0 && _abacusFeeMul1000<MAX_ABACUS_FEE_MUL_1000, "!fee<max");
-//     abacusFeeMul1000=_abacusFeeMul1000;
-//     /// @dev NOTICE! If you change the abacus fee, it does not change the custom abacus fees, so you must change those separately. This is to avoid large gas fees from looping through lists.
-// }
-
 /// @notice Function to set a custom abacus fee for specific wallet
 function setCustomAbacusFeeForEOA(address _address, address _token, uint _customFeeMul1000) external onlyOwner() {
     require(_customFeeMul1000<=MAX_ABACUS_FEE_MUL_1000);
@@ -227,26 +212,14 @@ function transferOwnership(address _newOwner) external onlyOwner() {
     _owner=_newOwner;
 }
 
-
-function checkApproved(address _token, uint _amount)external view returns (bool) {
-    uint256 amount = ERC20(_token).allowance(_token,address(this));
-    if (amount < _amount) {
-        return false;
-    }else{
-        return true;
-    }
-} 
-
-
-/// @notice returns sorted token addresses, used to handle return values from pairs sorted in this order, taken from univ2library
+/// @notice Returns sorted token addresses, used to handle return values from pairs sorted in this order. Code from the univ2library.
 function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
     }
 
-/// @notice From uniswap v2 library
-/// @notice given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+/// @notice Given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset. Code from the univ2library.
 function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure returns (uint amountOut) {
         require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
@@ -273,36 +246,5 @@ function mulDiv(uint256 x,uint256 y,uint256 denominator) internal pure returns (
     }
 }
 
-
-
-
-/// @dev FIXME: delete later, just for debugging
-
-function toString(address account) public pure returns(string memory) {
-    return toString(abi.encodePacked(account));
-}
-
-function toString(uint256 value) public pure returns(string memory) {
-    return toString(abi.encodePacked(value));
-}
-
-function toString(bytes32 value) public pure returns(string memory) {
-    return toString(abi.encodePacked(value));
-}
-
-function toString(bytes memory data) public pure returns(string memory) {
-    bytes memory alphabet = "0123456789abcdef";
-
-    bytes memory str = new bytes(2 + data.length * 2);
-    str[0] = "0";
-    str[1] = "x";
-    for (uint i = 0; i < data.length; i++) {
-        str[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
-        str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
-    }
-    return string(str);
-}
-
-   
 }
 
